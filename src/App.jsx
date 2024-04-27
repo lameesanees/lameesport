7d8427bd2156e92a39eea27483ce529b66009550
Lamees Anees <lamees.anees@gmail.com>
2024-04-27 17:56:08 +0530

    menu2

--- src/App.jsx
+++ src/App.jsx
@@ -43,12 +43,7 @@ function App() {
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={showMenu ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-              ></path>
+              
             </svg>
           </button>
         </div>
